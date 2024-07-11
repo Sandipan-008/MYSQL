dd67547ae094012e7cdaad23dea54fe04ca64f88
Sandipan Majumder <sandmaj2001@gmail.com>
2024-07-11 21:01:17 +0530

    Update and rename DATABASE_MANIPULATION to DATABASE_MANIPULATION.sql

---
+++ DATABASE_MANIPULATION.sql
@@ -0,0 +1,16 @@
+                                                                   --   Creation of Database
+
+syntyx:   create database database_name;
+example:  create database Office;
+
+
+                                                                   --  Displaying of Database
+
+syntyx: show databases;
+
+
+                                                                   --   Deleting of Database
+
+syntyx:    drop database database_name;
+example:   drop database Office;
+
