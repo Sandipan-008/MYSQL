c86adc0f3d67471912119dcaf4f8d4135e86de15
Sandipan Majumder <sandmaj2001@gmail.com>
2024-07-11 23:53:37 +0530

    Update 1.DATABASE_MANIPULATION.sql

--- 1.DATABASE_MANIPULATION.sql
+++ 1.DATABASE_MANIPULATION.sql
@@ -1,16 +1,25 @@
                                                                    --  Creation of Database
-
 syntyx:   create database database_name;
 example:  create database Office;
 
 
                                                                    --  Displaying of Database
-
 syntyx: show databases;
 
 
                                                                    --   Deleting of Database
-
 syntyx:    drop database database_name;
 example:   drop database Office;
 
+
+                                                                  --   Renaming Database
+
+ALTER DATABASE OldDatabaseName MODIFY NAME = NewDatabaseName;
+
+
+
+                                                                  --  Duplicating Database
+
+                                                
+
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>..............
