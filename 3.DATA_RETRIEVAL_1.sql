ccc258022c6e7e0479923a83307d6302ca1eb10f
Sandipan Majumder <sandmaj2001@gmail.com>
2024-07-11 23:26:04 +0530

    Update 3.DATA_RETRIEVAL_1.sql

--- 3.DATA_RETRIEVAL_1.sql
+++ 3.DATA_RETRIEVAL_1.sql
@@ -1,1 +1,13 @@
+                                                              -- INSERTING ROW/DATA
+
+SYNTYX:  INSERT INTO table_name
+         VALUES
+         (value1, value2, value3);
+example: INSERT INTO Office
+         VALUES
+         (4,'shyam','kolkata','564737299','60000','2001-08-09');
+
+
+
+                                                              -- DELETING ROW/DATA
 
