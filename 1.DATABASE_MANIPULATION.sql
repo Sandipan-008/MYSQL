e1ed61fdef6d3660e6659e327a51c0b3bede2611
Sandipan Majumder <sandmaj2001@gmail.com>
2024-09-06 22:30:48 +0530

    Update 1.DATABASE_MANIPULATION.sql

--- 1.DATABASE_MANIPULATION.sql
+++ 1.DATABASE_MANIPULATION.sql
@@ -4,8 +4,8 @@ example:  create database Office;
 
 
                                                                    --  Displaying of Database
-syntyx: show database database_name;
-example: show database Office;
+syntyx: show database like  'database_name%';
+example: show database like  'Office%';
 
 
                                                                    --   Deleting of Database
