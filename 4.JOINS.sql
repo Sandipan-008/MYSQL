d1c6853cd9b72e4194c5d465398e0eb754eef08d
Sandipan Majumder <sandmaj2001@gmail.com>
2024-12-16 22:40:09 +0530

    Update 4.JOINS.sql

--- 4.JOINS.sql
+++ 4.JOINS.sql
@@ -1,7 +1,39 @@
-SQL JOINS
-SQL INNER JOIN
+
+                                                  --SQL INNER JOIN
+table s = roll_no, name, address, phone_no, age
+table sc = roll_no, course_id
+  
+query: 
+select s.name, s.address, s.phone_no, sc.course_id
+from s INNER JOIN sc
+on s.roll_no = sc.roll_no;
+
+
+
+
+
+
 SQL OUTER JOIN
-SQL LEFT JOIN
+
+
+
+
+  
+                                                  --SQL LEFT JOIN
+table s = roll_no, name, address, phone_no, age
+table sc = roll_no, course_id
+
+query:
+select s.name, sc.course_id
+from s LEFT JOIN sc
+on sc.roll_no = s.roll_no;
+
+
+
+
+
+
+
 SQL RIGHT JOIN
 SQL FULL JOIN
 SQL SELF JOIN
