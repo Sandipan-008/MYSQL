904a7e689d8c9f02b1e355d50a05c48a903c5da4
Sandipan Majumder <sandmaj2001@gmail.com>
2024-12-17 20:46:53 +0530

    Update 4.JOINS.sql

--- 4.JOINS.sql
+++ 4.JOINS.sql
@@ -40,7 +40,9 @@ on sc.roll_no = s.roll_no
 
 
                                                    -- SQL SELF JOIN
-
+usage: retrive data from same table where one atrribute is connected more than one with another attribute
+example: select T1.sid from study as T1, study as T2
+where T1.sid = T2.sid AND T1.cid <> T2.cid;
                                                    -- SQL CROSS JOIN
 
                                                    -- SQL NATURAL JOIN
